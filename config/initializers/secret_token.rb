09ff7fb83433c784b9e2791ce2db6e14df1c70f5
Luis Cruz <luismirandacruz@gmail.com>
2014-01-08 10:44:52 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Morf::Application.config.secret_key_base = 'dea1b5f9c4f52ca881f567a1c78e5bbfef1066c14f3d94b45a7a19e45f8b28edd25e053da6cebb95e844f84e223286ff0398b8c6060a5c16f8b3beb6f7713c94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Morf::Application.config.session_store :cookie_store, key: '_morf_session'
