ccef1a1df2cb8a63868e4120514ee11f50807d5f
Luis Cruz <luismirandacruz@gmail.com>
2014-01-08 23:08:25 +0000

    added multi buildpack; installing all packages for R

--- init.r
+++ init.r
@@ -1,3 +1,8 @@
 # Example `init.r` file
 
 install.packages("tm", dependencies = TRUE)
+install.packages("e1071", dependencies = TRUE)
+install.packages("stringr", dependencies = TRUE)
+install.packages("SnowballC", dependencies = TRUE)
+install.packages("Rserve", dependencies = TRUE)
+
